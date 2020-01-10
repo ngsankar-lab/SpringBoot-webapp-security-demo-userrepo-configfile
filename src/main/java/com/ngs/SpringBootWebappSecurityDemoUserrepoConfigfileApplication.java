7821afd2353b5437182ce728faa6ac481ecf883c
Gowrisankar Nallamothu <gnallamoth@la-z-boy.com>
2020-01-10 13:32:19 -0500

    user repo

---
+++ src/main/java/com/ngs/SpringBootWebappSecurityDemoUserrepoConfigfileApplication.java
@@ -0,0 +1,13 @@
+package com.ngs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebappSecurityDemoUserrepoConfigfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebappSecurityDemoUserrepoConfigfileApplication.class, args);
+	}
+
+}
